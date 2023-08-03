48f9c0675dd4c9891b739c668d9d0518e73dfa41
Vikrant ruhela <vikrantrohilla607@gmail.com>
2023-08-03 15:36:14 +0530

    card component created

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import './App.css'
+import Card from './Components/Card'
 
 function App() {
 
   return (
-    <div>
-      hello
-    </div>
+    <>
+      <h1 className='text-5xl font-medium text-center p-8'>React Image Gallery</h1>
+      <div className='flex flex-wrap items-center justify-center w-full p-4 gap-4'>
+        <Card src={"https://images.unsplash.com/photo-1691036365017-76f8114f50c4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw3fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=500&q=60"} />
+      </div>
+    </>
   )
 }
 
---
+++ src/Components/Card.jsx
@@ -0,0 +1,9 @@
+
+
+const Card = ({ src }) => {
+    return (
+        <img src={src} alt="img" className="rounded-3xl w-72 h-72" />
+    )
+}
+
+export default Card
