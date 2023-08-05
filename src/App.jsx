17a10cc30557b59af071727088101ab3fa79ca2a
Vikrant ruhela <vikrantrohilla607@gmail.com>
2023-08-05 22:12:26 +0530

    final changes done , remaining just little error handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useEffect, useState } from 'react'
 import './App.css'
 import Card from './Components/Card'
-import CardDetail from './Components/CardDetail'
 import axios from "axios"
+import { Link } from 'react-router-dom'
+
 
 function App() {
   const [res, setRes] = useState([])
+
   async function downloadData() {
     const response = await axios.get("https://api.slingacademy.com/v1/sample-data/photos?limit=20")
     setRes(response.data.photos)
@@ -21,7 +23,7 @@ function App() {
     <>
 
       <div className='flex flex-wrap items-center justify-center w-full p-4 gap-4'>
-        {arr.map((ele, index) => { return <Card src={ele} key={index} /> })}
+        {arr.map((ele, index) => { return <Link key={index + 1} to={`/carddetails/${index + 1}`}><Card src={ele} /> </Link> })}
       </div>
     </>
   )
